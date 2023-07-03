ef89d8264f5e40d53ca0ce2877ee0e7c6b3330ba
Anirudh Sharma <anirudh1304@gmail.com>
2023-07-03 21:47:43 +0530

    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,43 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import { Routes } from "./routes";
+import { User } from "./entity/User";
 
-AppDataSource.initialize().then(async () => {
+function handleError(err, req, res, next) {
+  res.status(err.status || 500).send({ message: err.message });
+}
 
+AppDataSource.initialize()
+  .then(async () => {
     // create express app
-    const app = express()
-    app.use(bodyParser.json())
+    const app = express();
+    app.use(bodyParser.json());
 
     // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        async (req: Request, res: Response, next: Function) => {
+          try {
+            const result = await new (route.controller as any)()[route.action](
+              req,
+              res,
+              next
+            );
+            res.json(result);
+          } catch (error) {
+            next(error);
+          }
+        }
+      );
+    });
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
-
-    // setup express app here
-    // ...
+    app.use(handleError);
 
     // start express server
-    app.listen(3000)
+    app.listen(3000);
 
     // insert new users for test
     // await AppDataSource.manager.save(
@@ -39,6 +48,8 @@ AppDataSource.initialize().then(async () => {
     //     })
     // )
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
-
-}).catch(error => console.log(error))
+    console.log(
+      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+    );
+  })
+  .catch((error) => console.log(error));
